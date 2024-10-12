53929a2a1c5f7808eb182d678fdc56592bc2881d
Kader1680 <ouldhenniabaghdad@gmail.com>
2024-10-12 11:24:47 -0700

    upload requirement for the project

---
+++ index.py
@@ -0,0 +1,9 @@
+# generate random integer values
+from random import seed
+rom random import randint
+#f (1)
+# generate some integers
+for _ in range(10):
+	value = randint(0, 10)
+	print(value)seed random number generator
+seed
